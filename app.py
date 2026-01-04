85c8b7580fc433a6071d7aa1642ebfa7ab10bf18
Barış Ocak <barisocak214@gmail.com>
2026-01-04 15:11:30 +0300

    connect frontend to api service

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
+API_URL = "https://flask-render-docker.onrender.com/api/info"
+
+
 @app.route("/")
 def home():
-    return "Docker Flask çalışıyor - Barış Ocak"
+    r = requests.get(API_URL)
+    data = r.json()
+    return f"""
+    <h1>Mikro Hizmet Frontend</h1>
+    <p>API Servis: {data['service']}</p>
+    <p>Durum: {data['status']}</p>
+    <p>Sahip: {data['owner']}</p>
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
