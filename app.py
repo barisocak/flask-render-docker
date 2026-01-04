3dac71ef6137845dfa110207d5d70c948836fc1a
Barış Ocak <barisocak214@gmail.com>
2026-01-04 16:04:16 +0300

    add stateful api

--- app.py
+++ app.py
@@ -2,19 +2,24 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+counter = 0  # STATE (sunucu tarafında tutuluyor)
+
 @app.route("/")
 def home():
-    return "Stateless API çalışıyor - Barış Ocak"
+    return "Stateful API çalışıyor - Barış Ocak"
 
-@app.route("/stateless")
-def stateless():
+@app.route("/stateful")
+def stateful():
+    global counter
+    counter += 1
     return jsonify({
-        "message": "Stateless API çalışıyor",
+        "counter": counter,
         "owner": "Barış Ocak"
     })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
 
 
